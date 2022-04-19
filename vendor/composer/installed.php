6b3765147a0aa9968a4d17e28e362d4f8af7d546
Andrii Korzhov <sferaved@gmail.com>
2022-04-19 12:55:12 +0300

    Creat new project, new git branch for developes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fd13841847cdb80d78fcd7a2863fa9932bcdfa89',
+        'name' => 'vendor_name/test2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.6.2',
+            'version' => '3.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+            'dev_requirement' => true,
+        ),
+        'vendor_name/test2' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fd13841847cdb80d78fcd7a2863fa9932bcdfa89',
+            'dev_requirement' => false,
+        ),
+    ),
+);
