13d4b973c0b70c7347c6c30a1fb00cd06996f7d3
Andrii Korzhov <sferaved@gmail.com>
2022-04-29 14:42:10 +0300

    Bilding array of posts. Finish booistrap lessons

--- posts.php
+++ posts.php
@@ -2,26 +2,25 @@
 require_once "layouts/header.php";
 require_once "layouts/navigation.php";
 require_once "layouts/layout.php";
-require_once "layouts/footer.php"
+require_once "layouts/footer.php";
+require_once "posts_array.php";
 ?>
     <br>
     <?php
-    for ($i = 1; $i <= 8; $i++) { ?>
+
+    foreach ($postsArr as $item) { ?>
         <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1 bg-primary">
                 <img src="img/No_image_available.svg.png" alt="No foto" class="img-responsive img-rounded" height ="50" width="50" style="float: left">
-                Andrii Korzhov
+                <?php echo $item['author']; ?>
             </div>
 
-            <div class="col-md-10 col-md-offset-1 bg-info">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Quisque suscipit scelerisque odio, vel accumsan magna ultricies mollis.
-                Nunc in maximus purus, pretium vulputate ligula. Donec eleifend dolor ut fringilla lacinia.
-                Donec ut libero eros. Praesent vitae elementum nisl. Praesent semper euismod dapibus.
+            <div class="col-md-10 col-md-offset-1 bg-info text-justify">
+                <?php echo $item['post']; ?>
             </div>
             <div class="col-md-10 col-md-offset-1 bg-warning" >
-                12-05-2022
+                <?php echo $item['date']; ?>
             </div>
         </div>
         </div>
